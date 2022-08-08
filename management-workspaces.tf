4362b0520c758e3971b2f8f0b10814d0915b620b
H <hrvojep@yahoo.com>
2022-08-08 15:58:58 +1000

    update 3:58

--- management-workspaces.tf
+++ management-workspaces.tf
@@ -10,10 +10,10 @@
 #   }
 # }
 
-# locals {
-#   ##this can be changed to switch between A/B github connection.
-#   tfc_oauth_token = tfe_oauth_client.github-b.oauth_token_id
-# }
+locals {
+  ##this can be changed to switch between A/B github connection.
+  tfc_oauth_token = tfe_oauth_client.github-b.oauth_token_id
+}
 
 # provider "tfe" {
 #   token = var.tfe_token
@@ -68,13 +68,13 @@
 #   service_provider = "github"
 # }
 
-# resource "tfe_oauth_client" "github-b" {
-#   organization     = var.organization
-#   api_url          = "https://api.github.com"
-#   http_url         = "https://github.com"
-#   oauth_token      = var.github_personal_token
-#   service_provider = "github"
-# }
+resource "tfe_oauth_client" "github-b" {
+  organization     = var.organization
+  api_url          = "https://api.github.com"
+  http_url         = "https://github.com"
+  oauth_token      = var.github_personal_token
+  service_provider = "github"
+}
 
 # # resource "tfe_oauth_client" "gitlab-a" {
 # #   organization     = var.organization
