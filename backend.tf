60bb487f9295eb055107b7d3aed8ed9353826d0a
H <hrvojep@yahoo.com>
2022-08-08 11:48:48 +1000

    move org to vars

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "yulei"
+    organization = var.organization
 
     workspaces {
       name = "tfc-config-as-code"
